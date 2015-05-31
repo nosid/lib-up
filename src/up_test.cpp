34600e4a1cc6b9b88279902640cbc2e8f604dc96
Hubert Schmid <hubert.schmid@nosid.de>
2015-05-31 11:47:29 +0200

    up::test: Minimalistic unit testing framework. There is nothing special about this framework. It is just there to avoid dependencies on other libraries.

---
+++ src/up_test.cpp
@@ -0,0 +1,149 @@
+#include "up_test.hpp"
+
+#include <iostream>
+#include <mutex>
+#include <vector>
+
+#include "up_out.hpp"
+#include "up_utility.hpp"
+
+
+namespace
+{
+
+    class runner final
+    {
+    private: // --- scope ---
+        using self = runner;
+        using location = up_test::location;
+        using test = up_test::test_case_base;
+        static UP_WORKAROUND_THREAD_LOCAL self* top;
+    public:
+        static void check_passed(
+            location location __attribute__((unused)),
+            up::string_literal type __attribute__((unused)))
+        {
+            ++top->_passed;
+        }
+        static void check_failed(location location, up::string_literal type, up::fabrics fabrics)
+        {
+            ++top->_failed;
+            up::out(std::cerr, location.file(), ':', location.line(), ": failure: ", type, "\n");
+            for (auto&& fabric : fabrics) {
+                up::out(std::cerr, fabric, '\n');
+            }
+        }
+    private: // --- state ---
+        self* _parent;
+        test& _test;
+        std::size_t _passed = 0;
+        std::size_t _failed = 0;
+    public: // --- life ---
+        explicit runner(test& test)
+            : _parent(top), _test(test)
+        {
+            top = this;
+        }
+        runner(const self& rhs) = delete;
+        runner(self&& rhs) noexcept = delete;
+        ~runner() noexcept
+        {
+            top = _parent;
+        }
+    public: // --- operations ---
+        auto operator=(const self& rhs) & -> self& = delete;
+        auto operator=(self&& rhs) & noexcept -> self& = delete;
+        bool operator()() &&
+        {
+            try {
+                _test.run();
+            } catch (...) {
+                ++_failed;
+            }
+            return _failed == 0;
+        }
+    };
+
+    UP_WORKAROUND_THREAD_LOCAL runner* runner::top = nullptr;
+
+
+    class process final
+    {
+    public: // --- scope ---
+        using self = process;
+        using test = up_test::test_case_base;
+        static auto instance() -> auto&
+        {
+            static self instance;
+            return instance;
+        }
+    private: // --- state ---
+        std::mutex _mutex;
+        std::vector<test*> _tests;
+    private: // --- life ---
+        explicit process() = default;
+        process(const self& rhs) = delete;
+        process(self&& rhs) noexcept = delete;
+        ~process() noexcept = default;
+    public: // --- operations ---
+        auto operator=(const self& rhs) & -> self& = delete;
+        auto operator=(self&& rhs) & noexcept -> self& = delete;
+        void put(test* test)
+        {
+            std::unique_lock<std::mutex> lock(_mutex);
+            _tests.push_back(test);
+        }
+        auto main(int argc, char* argv[]) const -> int
+        {
+            up::out(std::cout, "TESTS:");
+            for (int i = 0; i < argc; ++i) {
+                up::out(std::cout, ' ', argv[i]);
+            }
+            std::cout << std::endl;
+            std::size_t failed = 0;
+            for (auto&& test : _tests) {
+                if (!runner{*test}()) {
+                    ++failed;
+                }
+            }
+            return failed == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
+        }
+    };
+
+}
+
+
+up_test::test_case_base::test_case_base(location location)
+    : _location(location), _armed(true)
+{
+    process::instance().put(this);
+}
+
+up_test::test_case_base::test_case_base(self&& rhs) noexcept
+    : _location(rhs._location), _armed(rhs._armed)
+{
+    rhs._armed = false;
+}
+
+void up_test::test_case_base::run()
+{
+    up::out(std::cout, "TEST[", _location.file(), ':', _location.line(), "]: ...\n");
+    _run();
+}
+
+
+void up_test::check::_passed(location location, up::string_literal type)
+{
+    runner::check_passed(location, type);
+}
+
+void up_test::check::_failed(location location, up::string_literal type, up::fabrics fabrics)
+{
+    runner::check_failed(location, type, std::move(fabrics));
+}
+
+
+auto up_test::test::main(int argc, char* argv[]) -> int
+{
+    return process::instance().main(argc, argv);
+}
---
+++ src/up_test.hpp
@@ -0,0 +1,153 @@
+#pragma once
+
+#include "up_fabric.hpp"
+#include "up_string_literal.hpp"
+
+/**
+ * This file provides a minimalistic unit testing framework to avoid
+ * dependencies on external libraries.
+ */
+
+namespace up_test
+{
+
+    class location final
+    {
+    private: // --- state ---
+        const char* _file;
+        std::size_t _line;
+        const char* _func;
+    public: // --- life ---
+        // (intentional) implicit constructor for brace initialization
+        location(const char* file, std::size_t line, const char* func)
+            : _file(file), _line(line), _func(func)
+        { }
+    public: // --- operations ---
+        auto file() const { return _file; }
+        auto line() const { return _line; }
+        auto func() const { return _func; }
+    };
+
+
+    class test_case_base
+    {
+    private: // --- scope ---
+        using self = test_case_base;
+    private: // --- state ---
+        location _location;
+        bool _armed;
+    protected: // --- life ---
+        explicit test_case_base(location location);
+        test_case_base(const self& rhs) = delete;
+        test_case_base(self&& rhs) noexcept;
+        ~test_case_base() noexcept = default;
+    public: // --- operations ---
+        auto operator=(const self& rhs) & -> self& = delete;
+        auto operator=(self&& rhs) & noexcept -> self& = delete;
+        void run();
+    private:
+        virtual void _run() = 0;
+    };
+
+
+    template <typename Callable>
+    class test_case final : private test_case_base
+    {
+    public: // --- scope ---
+        using self = test_case;
+    private: // --- state ---
+        Callable _callable;
+    public: // --- life ---
+        explicit test_case(location location, Callable&& callable)
+            : test_case_base(location), _callable(std::forward<Callable>(callable))
+        { }
+        test_case(const self& rhs) = delete;
+        /* Note that a move constructor is required due to the way the class
+         * is used in the below macro UP_TEST_CASE. */
+        test_case(self&& rhs) noexcept
+            : test_case_base(std::move(rhs))
+            , _callable(std::forward<Callable>(rhs._callable))
+        { }
+        ~test_case() noexcept = default;
+    public: // --- operations ---
+        auto operator=(const self& rhs) & -> self& = delete;
+        auto operator=(self&& rhs) & noexcept -> self& = delete;
+    private:
+        void _run() override
+        {
+            _callable();
+        }
+    };
+
+
+    class test_lambda_magic final
+    {
+    private: // --- state ---
+        location _location;
+    public: // --- life ---
+        explicit test_lambda_magic(location location)
+            : _location(location)
+        { }
+    public: // --- operations ---
+        template <typename Callable>
+        friend auto operator<<(test_lambda_magic&& magic, Callable&& callable)
+        {
+            return test_case<Callable>(magic._location, std::forward<Callable>(callable));
+        }
+    };
+
+
+    class check final
+    {
+    public: // --- scope ---
+        template <typename Actual, typename Expected>
+        static void equal(location location, Actual&& actual, Expected&& expected)
+        {
+            using namespace up::literals;
+            if (actual != expected || !(actual == expected)) {
+                _failed_aux(location, "equal"_s,
+                    std::forward<Actual>(actual),
+                    std::forward<Expected>(expected));
+            } else {
+                _passed(location, "equal"_s);
+            }
+        }
+    private:
+        static void _passed(location location, up::string_literal type);
+        static void _failed(location location, up::string_literal type, up::fabrics fabrics);
+        template <typename... Args>
+        static void _failed_aux(location location, up::string_literal type, Args&&... args)
+        {
+            _failed(location, type, {up::invoke_to_fabric_with_fallback(std::forward<Args>(args))...});
+        }
+    };
+
+
+    class test final
+    {
+    public: // --- scope ---
+        static auto main(int argc, char* argv[]) -> int;
+    };
+
+}
+
+namespace up
+{
+
+    using up_test::test;
+
+}
+
+
+#define UP_TEST_CONCATENATE_AUX(prefix, suffix) prefix ## suffix
+#define UP_TEST_CONCATENATE(prefix, suffix) UP_TEST_CONCATENATE_AUX(prefix, suffix)
+
+#define UP_TEST_CASE \
+    __attribute__((unused)) \
+    auto&& UP_TEST_CONCATENATE(UP_TEST_LAMBDA_, __COUNTER__) \
+        = ::up_test::test_lambda_magic({__FILE__, __LINE__, nullptr}) << []
+
+#define UP_TEST_EQUAL(...) \
+    do { \
+        ::up_test::check::equal({__FILE__, __LINE__, __PRETTY_FUNCTION__}, __VA_ARGS__); \
+    } while (false)
