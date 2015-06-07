f784ebc4d62f271d95279b0481962ca6fe7d6637
Hubert Schmid <hubert.schmid@nosid.de>
2015-06-07 23:20:19 +0100

    up_swap.hpp: Add code documentation (using doxygen syntax).

--- src/up_swap.hpp
+++ src/up_swap.hpp
@@ -1,39 +1,55 @@
 #pragma once
 
+/**
+ * @file
+ *
+ * @brief Header file to ease the usage and the implementation of @c swap
+ * functions (both for member and free functions).
+ */
+
 #include "up_include.hpp"
 
 /**
- * The following namespace is used to find and invoke the correct overloaded,
- * non-member function 'swap'. The candidate set will include functions found
- * via ADL as well as std::swap.
+ * @brief The namespace is used to find and invoke the proper non-member
+ * function @c swap. The candidate set will include functions found via ADL as
+ * well as @c std::swap.
  *
- * Using a dedicated namespace reduces the candidate set. It simplifies error
- * messages and avoids triggering an unintended conversion operation.
+ * Using a dedicated/isolated namespace reduces the candidate set. It
+ * simplifies error messages and avoids triggering an unintended conversion
+ * operation.
  */
 namespace up_adl_swap
 {
 
     using std::swap;
 
+    /**
+     * @private
+     *
+     * The function template invokes the best matching function of the
+     * candidate set, that consists of @c std::swap and all functions found
+     * via argument dependent lookup (ADL). ADL is intentionally used, so that
+     * types can provide an "overload" in their own namespace.
+     */
     template <typename... Args>
     auto invoke(Args&&... args)
         noexcept(noexcept(swap(std::declval<Args>()...)))
         -> decltype(swap(std::declval<Args>()...))
     {
-        /* The following function invocation (intentionally) supports ADL, so
-         * that 'swap' functions can be "overloaded" in the namespaces
-         * corresponding to the types. */
         return swap(std::forward<Args>(args)...);
     }
 
 }
 
+/// Designated namespace for the header file.
 namespace up_swap
 {
 
     /**
-     * Internal helper class to check if a swap member function exists, that
-     * can be invoked with the given arguments (using SFINAE). This class is
+     * @private
+     *
+     * Internal helper class to check if there is a proper swap member
+     * function, i.e. it can be invoked with the given arguments. The class is
      * required to choose between member and non-member function.
      */
     class invoke_swap_aux final
@@ -51,6 +67,9 @@ namespace up_swap
         template <typename... Args>
         static constexpr auto member() -> bool
         {
+            /* Prefer the member function if both exist. If neither exists,
+             * also prefer the member function, so that the compiler error
+             * message will point to the class with the missing overload. */
             return noexcept(test_member(std::declval<Args>()...))
                 || !noexcept(test_free(std::declval<Args>()...));
         }
@@ -58,6 +77,8 @@ namespace up_swap
 
 
     /**
+     * @private
+     *
      * Overloaded function template for invoking a member 'swap' function. It
      * will only take part in overload resolution if there is matching member
      * function.
@@ -74,6 +95,8 @@ namespace up_swap
     }
 
     /**
+     * @private
+     *
      * Overloaded function template for invoking a non-member 'swap' function.
      * It will only take part in overload resolution if there is no matching
      * member function.
@@ -90,6 +113,12 @@ namespace up_swap
     }
 
 
+    /**
+     * @private
+     *
+     * Apparently, the following function is neither in GCC nor Clang declared
+     * as @c noexcept (for no apparent reason).
+     */
     inline void invoke_swap(std::string& lhs, std::string& rhs) noexcept
     {
         /* Apparently the following function is not declared as noexcept,
@@ -97,19 +126,29 @@ namespace up_swap
         lhs.swap(rhs);
     }
 
+    /**
+     * @private
+     *
+     * WORKAROUND for Clang: The following function is not declared as @c
+     * noexcept in libc++ (for no apparent reason).
+     */
     template <typename... Types>
     void invoke_swap(std::unique_ptr<Types...>& lhs, std::unique_ptr<Types...>& rhs) noexcept
     {
-        /* Workaround for Clang: The following function is not declared as
-         * noexcept, for no apparent reason. */
         lhs.swap(rhs);
     }
 
 
     /**
-     * This function should be used instead of calling 'swap' directly. In
-     * addition to 'swap', it checks at compile-time whether the invoked swap
-     * function is declared as noexcept.
+     * @brief The function invokes 'swap' with several benefits compared to
+     * invoking 'std::swap' directly.
+     *
+     * First, it supports both member and non-member @c swap functions,
+     * preferring the former. Second, it checks at compile-time whether the
+     * actually invoked @c swap function is declared @c noexcept. And third,
+     * it avoids naming conflicts when invoking @c swap functions from the
+     * implementation of a @c swap function, because this function uses
+     * intentionally a different name.
      */
     template <typename... Args>
     void swap_noexcept(Args&&... args) noexcept
