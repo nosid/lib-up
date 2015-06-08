b529e2d6dc44797b2ea3d5a616455e46ca4bf09c
Hubert Schmid <hubert.schmid@nosid.de>
2015-06-08 23:18:44 +0100

    up::hash: Improved unit tests.

--- test/test_up_hash.cpp
+++ test/test_up_hash.cpp
@@ -5,7 +5,11 @@ namespace
 {
 
     UP_TEST_CASE {
-        UP_TEST_EQUAL(up::fnv1a("test", 4), 18007334074686647077u);
+        UP_TEST_EQUAL(up::fnv1a("test", 0), UINTMAX_C(14695981039346656037));
+        UP_TEST_EQUAL(up::fnv1a("test", 1), UINTMAX_C(12638201494206808739));
+        UP_TEST_EQUAL(up::fnv1a("test", 2), UINTMAX_C(632811855847011954));
+        UP_TEST_EQUAL(up::fnv1a("test", 3), UINTMAX_C(6261330701100204979));
+        UP_TEST_EQUAL(up::fnv1a("test", 4), UINTMAX_C(18007334074686647077));
     };
 
 }
