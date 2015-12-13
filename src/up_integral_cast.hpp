b1ce42543a51279301074b2e6e65bf72d723c9a7
Hubert Schmid <hubert.schmid@nosid.de>
2015-12-13 19:15:35 +0000

    up::integral_cast: Replace own implementation with __builtin_add_overflow, which provides reasonable good results both for Clang and GCC.

--- src/up_integral_cast.hpp
+++ src/up_integral_cast.hpp
@@ -8,8 +8,6 @@
  * different, even if they are identical on a particular platform.
  */
 
-#include <limits>
-
 #include "up_exception.hpp"
 
 namespace up_integral_cast
@@ -17,98 +15,44 @@ namespace up_integral_cast
 
     struct bad_integral_cast;
 
-    template <bool Value>
-    using constant = std::integral_constant<bool, Value>;
 
-    template <typename Target, typename Source>
-    auto integral_cast_aux_same_sign(Source value, std::true_type)
+    template <typename Result, typename Type>
+    auto integral_cast(Type value)
+        -> typename std::enable_if<std::is_integral<Result>{} && std::is_integral<Type>{}, Result>::type
     {
-        return static_cast<Target>(value);
-    }
-
-    template <typename Target, typename Source>
-    auto integral_cast_aux_same_sign(Source value, std::false_type)
-    {
-        if (value < Source{std::numeric_limits<Target>::min()}) {
-            UP_RAISE(bad_integral_cast, "bad-integral-cast"_s, value);
-        } else if (value > Source{std::numeric_limits<Target>::max()}) {
+        /* The following builtin is supported by both Clang and GCC, gives
+         * reasonable good results, and is likely to be improved with future
+         * compiler versions. */
+        Result result;
+        if (__builtin_add_overflow(value, Type(), &result)) {
             UP_RAISE(bad_integral_cast, "bad-integral-cast"_s, value);
         } else {
-            return static_cast<Target>(value);
+            return result;
         }
     }
 
-    template <typename Target, typename Source>
-    auto integral_cast_aux_from_signed_to_unsigned(Source value, std::true_type)
-    {
-        if (value < Source{0}) {
-            UP_RAISE(bad_integral_cast, "bad-integral-cast"_s, value);
-        } else {
-            return static_cast<Target>(std::make_unsigned_t<Source>(value));
-        }
-    }
 
-    template <typename Target, typename Source>
-    auto integral_cast_aux_from_unsigned_to_signed(Source value, std::true_type)
-    {
-        if (value > Source{std::numeric_limits<Target>::max()}) {
-            UP_RAISE(bad_integral_cast, "bad-integral-cast"_s, value);
-        } else {
-            return static_cast<Target>(value);
-        }
-    }
-
-    template <typename Target, typename Source, bool sign>
-    auto integral_cast_aux(Source value, constant<sign>, constant<sign>)
-    {
-        return integral_cast_aux_same_sign<Target>(value,
-            constant<sizeof(Target) >= sizeof(Source)>{});
-    }
-
-    template <typename Target, typename Source>
-    auto integral_cast_aux(Source value, std::false_type, std::true_type)
-    {
-        return integral_cast_aux_from_signed_to_unsigned<Target>(value,
-            constant<sizeof(Target) >= sizeof(Source)>{});
-    }
-
-    template <typename Target, typename Source>
-    auto integral_cast_aux(Source value, std::true_type, std::false_type)
-    {
-        return integral_cast_aux_from_unsigned_to_signed<Target>(value,
-            constant<sizeof(Target) <= sizeof(Source)>{});
-    }
-
-    template <typename Target, typename Source>
-    auto integral_cast(Source value)
-    {
-        static_assert(std::is_integral<Source>{}, "requires integral source type");
-        static_assert(std::is_integral<Target>{}, "required integral target type");
-        return integral_cast_aux<Target>(value, std::is_signed<Target>{}, std::is_signed<Source>{});
-    }
-
-
-    template <typename Source>
+    template <typename Type>
     class integral_caster_t final
     {
     private: // --- state ---
-        Source _value;
+        Type _value;
     public: // --- life ---
-        explicit integral_caster_t(Source value)
+        explicit integral_caster_t(Type value)
             : _value{value}
         { }
     public: // --- operations ---
-        template <typename Target>
-        operator Target() const &&
+        template <typename Result>
+        operator Result() const &&
         {
-            return integral_cast<Target>(_value);
+            return integral_cast<Result, Type>(_value);
         }
     };
 
-    template <typename Source>
-    auto integral_caster(Source value)
+    template <typename Type>
+    auto integral_caster(Type value)
     {
-        return integral_caster_t<Source>{value};
+        return integral_caster_t<Type>{value};
     }
 
 }
