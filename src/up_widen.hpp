9588951716d7c42d9547a1be24f5cdccd80c8ca6
Hubert Schmid <hubert.schmid@nosid.de>
2016-01-13 22:05:16 +0000

    up::widen: Refactor to provide better compiler error messages.
    This implementation should give better error message when the function
    template is called with unsupported types, i.e. it should clearly
    state that there is no matching function instead of displaying
    template instantiation errors.

--- src/up_widen.hpp
+++ src/up_widen.hpp
@@ -5,51 +5,39 @@
 namespace up_widen
 {
 
-    template <bool Condition, template <typename...> typename True, template <typename...> typename False, typename... Types>
-    class select;
-
-    template <template <typename...> typename True, template <typename...> typename False, typename... Types>
-    class select<true, True, False, Types...> : public True<Types...> { };
-
-    template <template <typename...> typename True, template <typename...> typename False, typename... Types>
-    class select<false, True, False, Types...> : public False<Types...> { };
-
-
-    template <bool True>
-    using bool_constant = std::integral_constant<bool, True>;
-
-    template <typename...>
-    using true_t = bool_constant<true>;
-
-    template <typename...>
-    using false_t = bool_constant<false>;
-
-
-    template <typename Type>
-    constexpr auto widest(Type value) -> std::enable_if_t<
-        select<std::is_integral<Type>::value, std::is_signed, false_t, Type>::value, std::intmax_t>
+    template <
+        typename Type,
+        typename = std::enable_if_t<std::is_integral<Type>::value && std::is_signed<Type>::value>>
+    constexpr auto widest(Type value) -> std::intmax_t
     {
         return {value};
     }
 
-    template <typename Type>
-    constexpr auto widest(Type value) -> std::enable_if_t<
-        select<std::is_integral<Type>::value, std::is_unsigned, false_t, Type>::value, std::uintmax_t>
+    template <
+        typename Type,
+        typename = std::enable_if_t<std::is_integral<Type>::value && std::is_unsigned<Type>::value>>
+    constexpr auto widest(Type value) -> std::uintmax_t
     {
         return {value};
     }
 
 
-    template <typename Result, typename Type>
-    using is_widen_base = bool_constant<
+    template <
+        typename Result,
+        typename Type,
+        bool = std::is_integral<Result>::value && std::is_integral<Type>::value>
+    class is_wider_integral final
+        : public std::integral_constant<
+        bool,
         std::is_signed<Result>::value == std::is_signed<Type>::value
         && widest(std::numeric_limits<Result>::min()) <= widest(std::numeric_limits<Type>::min())
-        && widest(std::numeric_limits<Result>::max()) >= widest(std::numeric_limits<Type>::max())>;
+        && widest(std::numeric_limits<Result>::max()) >= widest(std::numeric_limits<Type>::max())>
+    { };
 
     template <typename Result, typename Type>
-    using is_widen = select<
-        std::is_integral<Result>::value && std::is_integral<Type>::value,
-        is_widen_base, false_t, Result, Type>;
+    class is_wider_integral<Result, Type, false> final
+        : public std::integral_constant<bool, false>
+    { };
 
 
     template <typename Type>
@@ -64,7 +52,9 @@ namespace up_widen
             static_assert(noexcept(Type(value)), "requires noexcept");
         }
     public: // --- operations ---
-        template <typename Result, typename = std::enable_if_t<is_widen<Result, Type>::value>>
+        template <
+            typename Result,
+            typename = std::enable_if_t<is_wider_integral<Result, Type>::value>>
         constexpr operator Result() const noexcept
         {
             static_assert(noexcept(Result{_value}), "requires noexcept");
@@ -98,8 +88,14 @@ namespace up_widen
     };
 
 
-    template <typename Result = void, typename Type, typename = std::enable_if_t<
-        std::is_void<Result>::value ? std::is_integral<Type>::value : is_widen<Result, Type>::value>>
+    template <
+        typename Result = void,
+        typename Type,
+        typename = std::enable_if_t<
+            std::conditional_t<
+                std::is_void<Result>::value,
+                std::is_integral<Type>,
+                is_wider_integral<Result, Type>>::value>>
     auto widen(Type value)
     {
         return widen_t<Result>::widen(value);
