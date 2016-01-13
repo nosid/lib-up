4c58e90f34d3323e97833c2c50dd4be05872a19d
Hubert Schmid <hubert.schmid@nosid.de>
2016-01-13 19:53:39 +0000

    up::promote: Move std::enable_if_t into template type parameters.
    The previous way also worked. However, it caused confusing error
    messages, because std::make_signed was used on a non-integral type.
    The new way produces better error messages for unsupported types, i.e.
    overload disabled by std::enable_if_t.

--- src/up_promote.hpp
+++ src/up_promote.hpp
@@ -6,17 +6,15 @@ namespace up_promote
 {
 
     // integral promotion without changing signed-ness
-    template <typename Type>
-    auto promote(Type value)
-        -> std::enable_if_t<std::is_integral<Type>() && std::is_signed<Type>(), std::make_signed_t<decltype(+value)>>
+    template <typename Type, typename = std::enable_if_t<std::is_integral<Type>::value && std::is_signed<Type>::value>>
+    auto promote(Type value) -> std::make_signed_t<decltype(+value)>
     {
         return {value};
     }
 
     // integral promotion without changing signed-ness
-    template <typename Type>
-    auto promote(Type value)
-        -> std::enable_if_t<std::is_integral<Type>() && std::is_unsigned<Type>(), std::make_unsigned_t<decltype(+value)>>
+    template <typename Type, typename = std::enable_if_t<std::is_integral<Type>::value && std::is_unsigned<Type>::value>>
+    auto promote(Type value) -> std::make_unsigned_t<decltype(+value)>
     {
         return {value};
     }
